4cfddbb2b395894d056621e928d0a098f15b7269
Adam Lamson <adam.lamson@colorado.edu>
2020-04-21 10:37:05 -0600

    Added a prototype testing file for testing LUT integrand functions.

---
+++ tests/integrand_test.hpp
@@ -0,0 +1,12 @@
+/**
+ * @author      : adamlamson (adamlamson@LamsonMacbookPro)
+ * @file        : integrand_test
+ * @created     : Saturday Apr 18, 2020 14:00:44 MDT
+ */
+
+#ifndef INTEGRAND_TEST_HPP
+
+#define INTEGRAND_TEST_HPP
+
+#endif /* end of include guard INTEGRAND_TEST_HPP */
+
